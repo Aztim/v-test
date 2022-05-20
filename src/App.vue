6a01468ff141457a9d537418cbb9b9abee232897
Aztim <barboza-82@mail.ru>
2022-05-21 00:12:53 +0300

    my first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="catalog">
+    <div class="container">
+      <div class="catalog__filter">
+        <h1 class="catalog___filter--title">Добавление товара</h1>
+        <div class="catalog__filter--right"></div>
+      </div>
+      <router-view/>
+      </div>
+  </section>
+</template>
+
+<style lang="scss">
+
+</style>
